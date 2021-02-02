0417dad1674e9f5e8016711a23a9ffc7888673de
Thomas CORDONNIER <tcordonn@e2r4p14.42.fr>
2021-02-02 14:48:26 +0100

    utils update

--- includes/minishell.h
+++ includes/minishell.h
@@ -5,12 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: tcordonn <tcordonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/01/21 10:23:58 by tcordonn          #+#    #+#             */
-<<<<<<< HEAD
-/*   Updated: 2021/02/02 13:44:26 by gbabeau          ###   ########.fr       */
-=======
-/*   Updated: 2021/02/02 13:32:43 by tcordonn         ###   ########.fr       */
->>>>>>> e3d7b6281545087825a9868f70c9a9c4fcf1efbb
+/*   Created: 2021/02/02 13:55:17 by tcordonn          #+#    #+#             */
+/*   Updated: 2021/02/02 14:10:21 by tcordonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,15 +32,15 @@ typedef struct	s_main
 
 typedef struct	s_bi_parser
 {
-	char		*value;
-	s_bi_parser	*left;
-	s_bi_parser	*rigth;
-}			t_bi_parser;
+	char					*command;
+	struct	s_bi_parser		*left;
+	struct	s_bi_parser		*rigth;
+}				t_bi_parser;
 
-int		lexer(t_main *vars);
-t_bi_parser	*parser(char *vars);
-int		init_all(t_main	*vars);
-char	**ft_split_command(char const *str, char *charset);
-int		get_next_line(int fd, char **line);
+int				lexer(t_main *vars);
+t_bi_parser		*parser(char *vars);
+int				init_all(t_main	*vars);
+char			**ft_split_command(char const *str, char *charset);
+int				get_next_line(int fd, char **line);
 
 #endif
--- srcs/utils.c
+++ srcs/utils.c
@@ -6,36 +6,38 @@
 /*   By: tcordonn <tcordonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 11:23:32 by tcordonn          #+#    #+#             */
-/*   Updated: 2021/02/02 13:34:09 by tcordonn         ###   ########.fr       */
+/*   Updated: 2021/02/02 14:47:39 by tcordonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/include/libft.h"
 #include "../includes/minishell.h"
 
-static int		cpt(char const *str, char charset)
+static int		cpt(char const *str, char *charset)
 {
 	int	i;
-	int	a;
+	int	cpt;
 
 	i = 0;
-	a = 0;
+	cpt = 0;
 	while (str[i] != '\0')
 	{
-		while (str[i] == charset && str[i] != '\0')
+		while (ft_strncmp(&str[i], &charset[i], 1) && str[i] != '\0')
+		{}
+		/*while (str[i] == charset && str[i] != '\0')
 			i++;
 		if (str[i] != charset && str[i] != '\0')
 		{
 			a++;
 			while (str[i] != charset && str[i] != '\0')
 				i++;
-		}
+		}*/
+		i++;
 	}
-	i--;
-	return (a);
+	return (cpt);
 }
 
-static int		init(char **end, char const *str, char charset)
+static int		init(char **end, char const *str, char *charset)
 {
 	int	b;
 	int	a;
@@ -61,18 +63,19 @@ static int		init(char **end, char const *str, char charset)
 
 char			**ft_split_command(char const *str, char *charset)
 {
-	char	**end;
+	char	**tab;
 	int		i;
 	int		a;
 	int		b;
 
 	a = 0;
-	i = 0;
 	b = 0;
-	end = 0;
-	if (!(str) || !(end = malloc(sizeof(char*) * (cpt(str, charset) + 1))))
-		return (0);
-	if (!init(end, str, charset))
+	i = 0;
+	tab = NULL;
+	/*if (!(str) || !(end = malloc(sizeof(char*) * (cpt(str, charset) + 1))))
+		return (0);*/
+	print("%d", cpt(str, charset));
+	/*if (!init(end, str, charset))
 		return (0);
 	while (str[i] == charset && str[i] != '\0')
 		i++;
@@ -85,5 +88,11 @@ char			**ft_split_command(char const *str, char *charset)
 			i++;
 		end[a++][b] = '\0';
 	}
-	return (end);
+	return (end);*/
 }
+
+int		main()
+{
+	ft_split_command("ls| cat", " |"); // doit retourner [ls][|][cat]
+}
+		
