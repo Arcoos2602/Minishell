5f8531a92680cd763a4f9f1d5dbbfbc8be6501ea
Thomas CORDONNIER <tcordonn@e2r8p4.42.fr>
2021-02-09 16:27:19 +0100

    restart

--- srcs/lexer2.c
+++ srcs/lexer2.c
@@ -6,90 +6,84 @@
 /*   By: tcordonn <tcordonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/02 11:23:32 by tcordonn          #+#    #+#             */
-/*   Updated: 2021/02/05 13:33:29 by tcordonn         ###   ########.fr       */
+/*   Updated: 2021/02/09 16:26:10 by tcordonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft/include/libft.h"
 #include "../includes/minishell.h"
 
-int				check_char(char	c)
+/*if (ft_iswhitespace(str[i]))
+			{
+				printf("check");
+				while (ft_iswhitespace(str[i]) && str[i] != '\0')
+				{
+					i++;
+				}
+				cpt++;
+}*/
+
+int				separators(char	c)
 {
-	if (c >= 33 && c <= 126)
+	if (c == '|' || c == '>' || c == '<')
 		return (1);
 	return (0);
 }
 
-static char		*ft_free(char **tab, int index)
+int				cpt(char *str)
 {
-	int	i;
-
-	i = 0;
-	while (i < index)
-	{
-		free(tab[i]);
-		i++;
-	}
-	free(tab);
-	return (NULL);
-}
-
-int		separators(char c, char c_next) // str to check >>
-{
-	if (c == '|' || c == '>' || c == '<' || c == '"' || (c == '>' && c_next == '>'))
-		return (1);
-	return (0);
-}
-
-static int		cpt(char const *str)
-{
-	int	i;
-	int	cpt;
+	int		i;
+	int		cpt;
 
 	i = 0;
 	cpt = 0;
-	while (str[i] != '\0')
+	while (str[i])
 	{
-		while (ft_iswhitespace(str[i++]))
-			;
-		while (check_char(str[i]))
+		while (ft_iswhitespace(str[i]) && str[i] != '\0')
 			i++;
-		if (separators(str[i], str[i + 1]))
-			
-			
-		cpt++;
-	}
-	return (cpt);
-}
-
-static int		init_tab(char **tab, char const *str)
-{
-	int	x;
-	int	y;
-	int	i;
-
-	i = 0;
-	x = 0;
-	y = 0;
-	/*while (x < cpt(str))
-	{
-		y = 0;
-		while (ft_iswhitespace(str[i++]) && str[i] != '\0')
-			;
-		while (str[i] != '\0' && check_char(str[i]) && no_separators(str[i], str[i + 1]))
+		while (separators(str[i]) == 0 && str[i] != '\0')
 		{
-			y++;
+			if (ft_iswhitespace(str[i]))
+			{
+				while (ft_iswhitespace(str[i]))
+					i++;
+				cpt++;
+			}
 			i++;
 		}
-		if (!(tab[x] = (char*)malloc(sizeof(char) * (y + 1))))
-			ft_free(tab, i);
-		x++;
-	}*/
-	tab[x] = 0;
+		cpt++;
+		if (separators(str[i]))
+			cpt++;
+		i++;
+	}
+	return (cpt);	
+}
+
+int				init_tab(char **tab, char *str)
+{
+	int		x;
+	int		i;
+	int		size_line;
+
+	x = 0;
+	y = 0;
+	i = 0;
+	size_line = 0;
+	while (str[i++])
+	{
+		while (ft_iswhitespace(str[i]) && str[i] != '\0')
+			i++;
+		while (separators(str[i]) == 0 && str[i] != '\0')
+		{
+			i++;
+		}
+		if (!(tab[x] = (char*)malloc(sizeof(char) * (size_line + 1))))
+			return (0);
+	}
 	return (1);
 }
 
-char			**token(char *str) // needs ton handle "" >> ''
+char			**token(char *str)
 {
 	char	**tab;
 	int		i;
@@ -101,33 +95,15 @@ char			**token(char *str) // needs ton handle "" >> ''
 	y = 0;
 	i = 0;
 	tab = NULL;
-	printf("%d", cpt(str) + 1);
-	//quote(str);
+	/*if (!(quote(str)))
+		return (NULL);*/
+	if (cpt(str) == 0)
+		return (NULL);
+	printf("%d", cpt(str));
+	//mutli_pipe
 	if (!(str) || !(tab = malloc(sizeof(char *) * (cpt(str) + 1))))
 		return (0);
 	init_tab(tab, str);
-	/*while (ft_iswhitespace(str[i]) && str[i] != '\0')
-		i++;
-	while (str[i] != '\0')
-	{
-		y = 0;
-		check = 0;
-		while (ft_iswhitespace(str[i]) && str[i] != '\0')
-			i++;
-		if (no_separators(str[i], str[i + 1]) == 0) // fonctionne pas pour ">>"
-		{
-			tab[x][y] = str[i];
-			y++;
-			i++;
-		}
-		while (str[i] != '\0' && check_char(str[i]) && no_separators(str[i], str[i + 1]))
-		{
-			tab[x][y++] = str[i++];
-		}
-		if (ft_iswhitespace(str[i - 1]))
-			;
-		tab[x++][y] = '\0';
-	}*/
 	return (tab);
 }
 
@@ -154,9 +130,10 @@ int main()
 {
 	char	**lexer;
 
-	lexer = token("ls | cat cdawdwa|dwadwa"); // espace à la fin bugged
-	print_tab(lexer);
+	lexer = token("ls | cat");
+	//print_tab(lexer);
 	return (1);
 }
 
 /// separateurs à gérer :			; ' "" > < >> '
+///// """"""
