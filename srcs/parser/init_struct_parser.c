5473876f66a25d8a31d439c77cfd4f62b73b093c
pazteek <babeau.gauthier@gmail.com>
2021-02-13 14:59:13 +0100

    version final 1

---
+++ srcs/parser/init_struct_parser.c
@@ -0,0 +1,67 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init_struct.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbabeau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/09 10:26:51 by gbabeau           #+#    #+#             */
+/*   Updated: 2021/02/09 15:40:58 by gbabeau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libft/include/libft.h"
+#include "../../includes/minishell.h"
+
+t_parser	*add_parser(t_parser *parser, t_parser *next)
+{
+	if (parser == NULL)
+		parser = next;
+	if (parser->next == NULL)
+		parser->next = next;
+	else
+		add_parser(parser->next, next);
+	return (next);
+}
+
+t_parser	*parser_new(char **lexer, int *i)
+{
+	t_parser	*parser;
+
+	if (0 == (parser = malloc(sizeof(t_parser))))
+		return (NULL);
+	parser->pipe = NULL;
+	parser->next = NULL;
+	if (0 == (parser->pipe = init_pipe(lexer, i)))
+		return (NULL);
+	return (parser);
+}
+
+t_parser	*init_parser(char **lexer, int *i)
+{
+	int			nbr;
+	t_parser	*parser;
+
+	*i = 0;
+	parser = NULL;
+	nbr = nbr_command_line(lexer);
+	printf("nombre command  = %d\n", nbr);
+	while (nbr != 0)
+	{
+		if (parser == NULL)
+		{
+			if (0 == (parser = parser_new(lexer, i)))
+				return (NULL);
+		}
+		else if (0 == add_parser(parser, parser_new(lexer, i)))
+			return (NULL);
+		nbr--;
+		if (nbr != 0)
+		{
+			while (lexer[*i][0] != ';')
+				*i += 1;
+			*i += 1;
+		}
+	}
+	return (parser);
+}
---
+++ srcs/parser/parser_comand.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parser_comand.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gbabeau <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/09 14:04:46 by gbabeau           #+#    #+#             */
+/*   Updated: 2021/02/09 14:45:36 by gbabeau          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../libft/include/libft.h"
+#include "../../includes/minishell.h"
+
+char		**init_command_pipe(char **command, char **lexer, int *i)
+{
+	int	size;
+
+	size = nbr_words_exe(lexer, i);
+	printf("longueur comand = %d\n", size);
+	if (NULL == (command = malloc_tb_str(command, &lexer[*i - size], size)))
+		return (NULL);
+	return (command);
+}
+
+int			nbr_command_line(char **lexer)
+{
+	int		i;
+	int		nbr_command_line;
+
+	nbr_command_line = 1;
+	i = 0;
+	while (lexer[i] != NULL)
+		if (lexer[i++][0] == ';')
+		{
+			if (lexer[i] != NULL && ft_compare_c_to_s(lexer[i][0], ";|"))
+				return (0);
+			nbr_command_line++;
+		}
+	return (nbr_command_line);
+}
