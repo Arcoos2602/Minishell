ad9d674983de9da3c665840628c8a9831d1a1141
Thomas CORDONNIER <tcordonn@e2r6p4.42.fr>
2021-02-25 15:51:08 +0100

    does it works ?

--- srcs/builtins/echo.c
+++ srcs/builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: tcordonn <tcordonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/24 10:24:14 by tcordonn          #+#    #+#             */
-/*   Updated: 2021/02/25 11:53:11 by tcordonn         ###   ########.fr       */
+/*   Updated: 2021/02/25 14:36:25 by tcordonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,4 +18,3 @@ int		ft_echo(char **arg, int	i)
 	check_arg(arg[i++]);
 	ft_putstr_fd(arg, 1);
 }
-
