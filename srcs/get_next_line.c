8339d1f407fe0c4ed4393a72404ef29e5817d6e6
Thomas CORDONNIER <tcordonn@e2r8p4.42.fr>
2021-09-27 18:04:52 +0200

    get

--- srcs/get_next_line.c
+++ srcs/get_next_line.c
@@ -6,75 +6,37 @@
 /*   By: tcordonn <tcordonn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/21 14:49:55 by tcordonn          #+#    #+#             */
-/*   Updated: 2021/09/27 12:44:28 by tcordonn         ###   ########.fr       */
+/*   Updated: 2021/09/27 18:04:04 by tcordonn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/include/libft.h"
-#include "../includes/minishell.h"
+#include "get_next_line.h"
 
-int		ft_struct(char *str, char **line)
+int ft_gnl_rec(char **line, int index)
 {
-	char	*tmp;
-	int		len;
-
-	len = 0;
-	while (str[len] != '\0' && str[len] != '\n')
-		len++;
-	if (str[len] == '\n')
-	{
-		*line = ft_substr(str, 0, len);
-		tmp = ft_strdup(&str[len + 1]);
-		free(str);
-		str = tmp;
-	}
-	else
-	{
-		*line = ft_substr(str, 0, len);
-		free(str);
-		str = NULL;
-		return (0);
-	}
-	return (1);
+    int ret;
+    char buf;
+    
+    ret = read(0, &buf, 1);
+    if (ret == -1)
+        return (-1);
+    if (ret == 1 && buf != '\n')
+    {
+        if ((ret = ft_gnl_rec(line, index + 1)) == -1)
+            return (-1);
+        (*line)[index] = buf;
+    }
+    else
+    {
+        (*line) = (char *)malloc(sizeof(char) * (index + 1));
+        if (!(*line))
+            return (-1);
+        (*line)[index] = '\0';
+    }
+    return (ret);
 }
 
-int		ft_return(char *str, char **line, int ret)
+int    get_next_line(char **line)
 {
-	if (ret < 0)
-		return (-1);
-	else if ((ret == 0 && str == NULL))
-	{
-		*line = ft_strdup("");
-		return (0);
-	}
-	else
-		return (ft_struct(str, line));
-}
-
-int		get_next_line(int fd, char **line)
-{
-	int			ret;
-	char		buff[BUFFER_SIZE + 1];
-	static char	*str;
-	char		*tmp;
-
-	if (BUFFER_SIZE <= 0)
-		return (-1);
-	if (fd < 0 || line == NULL)
-		return (-1);
-	while ((ret = read(fd, buff, BUFFER_SIZE)) > 0)
-	{
-		buff[ret] = '\0';
-		if (str == NULL)
-			str = ft_strdup(buff);
-		else
-		{
-			tmp = ft_strjoin(str, buff);
-			free(str);
-			str = tmp;
-		}
-		if (ft_strchr(str, '\n'))
-			break ;
-	}
-	return (ft_return(str, line, ret));
+    return (ft_gnl_rec(line, 0));
 }
